6a5e285be949b91b8f00ec61218576370947d68a
shaunberkley <shaun@berkleysoftware.com>
2024-07-08 12:13:57 -0700

    Fix app height

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Chat } from '@/components/chat';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-[100dvh] flex-col items-center justify-between">
       <div className="flex flex-1 w-full">
         <Chat />
       </div>
